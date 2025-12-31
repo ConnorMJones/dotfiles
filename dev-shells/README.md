dfeaa38ed85d5666ddf27af9b13a1593f5e33d9d
Connor Jones <connorj@live.ca>
2025-12-31 00:28:00 -0400

    Shell functions to replace alias.

--- dev-shells/README.md
+++
@@ -1,3 +0,0 @@
-### NIX SHELLS
-These are needed since you can't just use normal dev tools globally like pkg-config and what not. 
-These are put here so I don't bloat projects with random nix files and flakes, you can just alias into the right environment to get the extra pkgs you need. Rust should have everything any random rust project I have should need.
--- modules/shell.nix
+++ modules/shell.nix
@@ -1,18 +1,25 @@
 { config, pkgs, ... }:
-let
-  myAlias = {
-    "vim" = "nvim";
-    v = "nvim";
-    ll = "ls -la";
-    #rsh = "nix-shell ~/.dotfiles/dev-shells/rust.nix --command fish";
-    rsh = "nix develop ~/.dotfiles/dev-shells/rust -c fish";
-  };
-in
 {
-  programs = {
-    fish = {
-      enable = true;
-      shellAliases = myAlias;
+  programs.fish = {
+    enable = true;
+    shellAliases = {
+      vim = "nvim";
+      v = "nvim";
+      ll = "ls -la";
     };
+
+    shellInit = ''
+      function sh
+          if test (count $argv) -eq 0
+              echo "Usage: sh <shell>"
+              echo "Available shells:"
+              echo "  rust zig python lean full"
+              return 1
+          end
+
+          set shell $argv[1]
+          nix develop ~/.dotfiles/dev-shells#$shell -c fish
+      end
+    '';
   };
 }
