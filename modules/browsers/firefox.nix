b348df6cbe409e9a3c6f2596f4b79c1f2096905d
Connor Jones <connorj@live.ca>
2026-05-01 12:40:51 -0300

    remove foxy proxy.

--- modules/browsers/firefox.nix
+++ modules/browsers/firefox.nix
@@ -44,11 +44,11 @@
             "https://addons.mozilla.org/firefox/downloads/latest/uBlock0@raymondhill.net/latest.xpi";
         };
         # Foxy Proxy
-        "foxyproxy@eric.h.jung" = {
-          "installation_mode" = "normal_installed";
-          "install_url" =
-            "https://addons.mozilla.org/firefox/downloads/latest/foxyproxy@eric.h.jung/latest.xpi";
-        };
+        # "foxyproxy@eric.h.jung" = {
+        #   "installation_mode" = "normal_installed";
+        #   "install_url" =
+        #     "https://addons.mozilla.org/firefox/downloads/latest/foxyproxy@eric.h.jung/latest.xpi";
+        # };
         # Yomitan - popup dictionary for language learning
         "{6b733b82-9261-47ee-a595-2dda294a4d08}" = {
           "installation_mode" = "normal_installed";
