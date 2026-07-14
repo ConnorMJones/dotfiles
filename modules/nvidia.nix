a01c757cc05ad9994a0fe36e8bdefbb14542f0cc
Connor Jones <connorj@live.ca>
2026-07-14 11:55:39 -0300

    Update nvidia settings for faulty display management Remove foliate

--- modules/nvidia.nix
+++ modules/nvidia.nix
@@ -12,8 +12,9 @@
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.nvidia = {
     modesetting.enable = true;
+    powerManagement.enable = true;
     powerManagement.finegrained = false;
-    open = false;
+    open = true;
     nvidiaSettings = true;
     package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
